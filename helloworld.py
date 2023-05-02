6e7c11639ba32ab86ba8825f94e01bc98cccef66
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-02 16:43:55 +0530

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+def myFunction() :
+    User=input("Enter your Name: ");
+    Profession=input("Enter your profession you interested: ")
+    print("Your name is" ,User, "," "Your profession is:",Profession)
+
+myFunction()    
