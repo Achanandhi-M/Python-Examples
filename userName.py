a4580475538922bfe0bac68c7c45ea78e2af4aff
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-03 21:54:59 +0530

    next iteration

---
+++ userName.py
@@ -0,0 +1,51 @@
+def signIn(username2,password2):
+    username1=input('Enter the Username: ')
+    password1=input('Enter the password : ')
+    if(username2 == username1 and password2 == password1 ):
+        print('Thank you')
+    else:
+        print('Its seems your crenditals are wrong')
+        creditsSetup=int(input('press 0 for signUp and 1 for logout: '))
+        if(creditsSetup==0):
+            signUp()
+        elif(creditsSetup==1):
+            print('See you soon :)')   
+        else:
+            print('Invalid choice please Enter correct option')               
+
+def signUp():
+    username2=input('Enter the Username: ')
+    password2=input('Enter the password :')
+    userChoice=input('Who is your favourite person :')
+    interest=int(input("press 0 for signIn and 1 for exit: ?"))
+    if(interest==0):
+        signIn(username2,password2)
+    elif(interest==1):
+        print('Will see you soon:) ')  
+    else:
+        print('Invalid choice please Enter correct option') 
+        signUp()     
+
+# The program starts Here
+print('Welcome')
+#variable declaration
+username1=''
+password2=None
+username2=None
+password1=''
+interest=0
+creditsSetup=0
+# First function Execution
+def myOption():
+    option = int(input('Press 0 for signIn and 1 for signUp: '))
+    if option == 0:
+        signIn(username2, password2)
+
+    elif option == 1:
+        signUp()
+
+    else:
+        print('Invalid choice please Enter correct option ')
+        myOption()      
+
+myOption()        
---
+++ whileLoop.py
@@ -0,0 +1,9 @@
+birthYear=0;#Assuming our initial age as 0 when we born
+godChoice=int(input("The God choice is :")) #Assume you as a God and Enter the value,But in the real world we cannot know the value
+while (birthYear<=godChoice):
+    print("Your age is", birthYear)
+    if(birthYear != godChoice):
+        print("Enjoy your Life") 
+    else:
+        print("God decided to give rest to your soul ")    
+    birthYear+=1 
