0c3eda2cbcad11dc775cd77a6262859affe7a1d6
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-04 20:19:21 +0530

    SecondDay

--- whileLoop.py
+++ whileLoop.py
@@ -1,5 +1,7 @@
 birthYear=0;#Assuming our initial age as 0 when we born
+
 godChoice=int(input("The God choice is :")) #Assume you as a God and Enter the value,But in the real world we cannot know the value
+
 while (birthYear<=godChoice):
     print("Your age is", birthYear)
     if(birthYear != godChoice):
