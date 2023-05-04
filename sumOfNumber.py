7460a599a4db4a6c1a7e268070975470d77584fc
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-04 20:13:11 +0530

    new commit

---
+++ sumOfNumber.py
@@ -0,0 +1,10 @@
+sum=0
+n=int(input('Enter the value:'))
+for i in range(0,n+1):
+    sum=sum+i
+    
+if(sum<=1):
+    print('Sum of',n, 'value is' , sum)
+else:
+     print('Sum of',n, 'values is' , sum)
+
---
+++ switchCase.py
@@ -0,0 +1,24 @@
+import datetime
+now=datetime.datetime.now()
+currentDay=now.strftime("%A")
+match currentDay:
+    case "Monday":
+        print("Current Day is" ,currentDay)
+
+    case "Tuesday":
+        print("Current Day is" ,currentDay)
+
+    case "Wednesday":
+        print("Current Day is" ,currentDay)
+
+    case "Thursday":
+        print("Current Day is" ,currentDay)
+
+    case "Friday":
+        print("Current Day is" ,currentDay)
+
+    case "Saturday":
+        print("Current Day is" ,currentDay)
+        
+    case "Sunday":
+        print("Current Day is" ,currentDay)
