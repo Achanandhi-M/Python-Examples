c13eb25378731f4761476e267a387074cd435c80
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-04 20:18:05 +0530

    FirstDay

--- listExamples.py
+++ listExamples.py
@@ -1,7 +1,8 @@
 myFirstList=[1,2,3,4,5]
 mySecondList=[1,2,3,4,5]
 result=[]
+
 for i in range (len(myFirstList)):
     result.append(myFirstList[i] *  mySecondList[i])
+    
 print(result)
-print(len(result))
