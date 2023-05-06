adf8aad9a5dc02901793fbe064e4f904556497b2
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-06 20:57:47 +0530

    FifthDay

--- ArmStrong.py
+++ ArmStrong.py
@@ -1,11 +1,12 @@
+
 userInput=int(input('Enter the value :'))
 sum=0
 length=len(str(userInput))
 FinalValue=userInput
 while userInput >0:
-    modValue=userInput%10
-    sum+=modValue ** length
-    userInput//=10
+    modValue=userInput%10 # Get the last digit of the number
+    sum+=modValue ** length # Add the digit raised to the power of the number of digits to the sum
+    userInput//=10 # Remove the last digit from the number
 
 if FinalValue == sum:
     print('Given Number is Armstrong')
--- swap.py
+++ swap.py
@@ -1,7 +1,9 @@
 a=int(input('Enter the value of a : '))
 b=int(input('Enter the value of b:'))
+
 print('The value of A before swapping ' ,a )
 print('The value of B before swapping' , b)
+
 a,b=b,a # simple way to swap two values :)
 print('The value of A After swapping ' ,a )
 print('The value of B After swapping' , b)
