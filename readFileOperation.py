5cc50cc34e4744a41d967b6b07297b0fd5c7e3f9
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-07 13:28:46 +0530

    SixthDay

---
+++ readFileOperation.py
@@ -0,0 +1,3 @@
+file=open('file.txt','r')
+f=file.read()
+print(f)
---
+++ writeFileOperations.py
@@ -0,0 +1,4 @@
+file=open('file.txt' ,'w')
+file.write('Python is famous programming Language\n')
+file.write('Mojo is superset of Python\n')
+file.write('Computer science')
