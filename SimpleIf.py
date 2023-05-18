d8f6c27de59226b9f7962a766bf6ac4d9714f8bc
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-18 15:55:14 +0530

    Seventeenth Day

---
+++ SimpleIf.py
@@ -0,0 +1,22 @@
+class Fun:
+    def __init__(self,mind):
+        self.mind=mind
+
+    def boring(self):
+        print('Take rest :)')    
+
+    def active(self):
+        print('Enjoy your work') 
+
+def userInput():
+    mind=int(input('Enter 0 for boring mindset and 1 for active mindset : '))
+    myobj=Fun(mind)
+    if mind == 0:
+        myobj.boring()
+    elif mind == 1:
+        myobj.active()    
+    else:
+        print('Your not in the universe')
+        userInput()     
+
+userInput()        
