67becabb9ab178a22ddfbddb436a2dff909de0a7
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-21 09:46:12 +0530

    EighteenthDay

---
+++ myRoutine.py
@@ -0,0 +1,12 @@
+morningWakeUp=int(input('Enter your Wake up time:'))
+ProperExercise=int(input('Enter your workout time:'))
+ProperFood=input('Have you consumed a food lot (yes or no):')    
+
+
+if(morningWakeUp <= 5  and ProperExercise <= 20  and ProperFood == 'no'):
+    print('Conquer the day :)')
+else:
+    print('Figure out your mistake and execute properly')    
+
+
+
