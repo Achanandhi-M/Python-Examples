c9e18a87461a784afaf165d5617dd5fb64b78500
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-23 07:05:17 +0530

    TwentyTwoDay

---
+++ Typehints.py
@@ -0,0 +1,7 @@
+def addTwoNumber(a:int , b:int) -> int:
+    #Type hints in Python allow you to provide hints or annotations about the expected types of variables, function arguments, and function return values
+    return a+b
+
+
+result=addTwoNumber(10, 20)
+print(result)    
