2475c827721181d1190bc1088d9fdea386f3e48d
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-26 09:08:05 +0530

    TwentyFiveDay

---
+++ FormattingString.py
@@ -0,0 +1,6 @@
+firstName=input('Enter your First Name :')
+LastName=input('Enter your Last Name: ')
+
+name= f"{firstName} {LastName}" 
+#f "" is used for formatting
+print("Your Full name :" ,name )
