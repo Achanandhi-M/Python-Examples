e835fbe64b6b0562b01f14352a6dc94ca0529fb4
Achanandhi24 <achanandhi.m@gmail.com>
2023-05-29 18:47:27 +0530

    TwentyEightDay

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n==0:
+        return 1
+
+    else:
+        return n * factorial(n-1)
+
+
+n=int(input('Enter the value :'))            
+answer=factorial(n)
+print('The Factorial of ' , n ,'is', answer)
